292faf0497cad6de72dfa33a9e8d6dff525ed918
Matt_Windows <218104632+mabstract25@users.noreply.github.com>
2026-09-18 11:25:51 +0100

    playGame and createPlayer functions

--- tictactoe.js
+++ tictactoe.js
@@ -1,4 +1,4 @@
-function createGameBoard(name) { 
+const createGameBoard = (() => { 
     const rows = 3;
     const cols = 3;
     let gameBoard = [];
@@ -9,22 +9,33 @@ function createGameBoard(name) {
             gameBoard[i].push("");
         }
     }
+    
+    return gameBoard;
+})();
 
-    return {name, gameBoard};
+let token = "";
+
+function createPlayer(name, token) {
+    
+    return {
+        name,
+        greeting() {
+            console.log(`${name}, your token is ${token}`)
+        },
+        token};
 }
 
-const playerOne = {
+const playerOne = createPlayer("Player One","X")
+const playerTwo = createPlayer("Player Two","O")
+playerOne.greeting();
+playerTwo.greeting();
 
-}
 
-const playerTwo = {
+function playGame(row,column) {
+    playerToken = token;
+    createGameBoard[row].splice(column, 1, playerToken)
 
-}
-
-function playGame() {
-
+    console.log(createGameBoard[row]);
 } 
 
-const matt = createGameBoard("Matt");
-
-console.log(matt)
+console.log(createGameBoard);
